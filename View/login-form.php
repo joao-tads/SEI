9fff11210c139930b2ce7937ec5c4741eccd1fc1
joao-tads <55167438+joao-tads@users.noreply.github.com>
2019-11-08 21:14:19 -0200

    first commit

---
+++ View/login-form.php
@@ -0,0 +1,5 @@
+<form action="/login" method="post">
+    <input type="email"  name="email" placeholder="email" required autofocus>
+    <input type="password"  name="senha" placeholder="senha" required>
+    <button type="submit">Login</button>
+</form>
