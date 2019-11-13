46ebf67ac03e5a689654a24d3ce93174c2910d24
joao-tads <55167438+joao-tads@users.noreply.github.com>
2019-11-12 22:40:49 -0200

    update ultima aula

--- database/Projeto.sql
+++ database/Projeto.sql
@@ -4,25 +4,25 @@ create database Projeto;
 use Projeto;
 
 create table Funcionario (
-	MatriculaFuncionario int primary key not null auto_increment,
+	Matricula int primary key not null auto_increment,
     nome varchar(100) not null,
     idade int not null,
     email varchar(50) not null,
     usuario varchar(20) not null,
-    senha varchar(20) not null
+    senha varchar(100) not null
 );
 
 create table Aluno (
-    idAluno int primary key not null auto_increment,
+    Matricula int primary key not null auto_increment,
     nome varchar(50) not null,
     email varchar(50) not null,
     usuario varchar(50) not null,
-    senha varchar(10) not null
+    senha varchar(100) not null
 
 );
 
-insert into Funcionario (Nome, idade, usuario, senha) values('Joao Guedes', 24, 'jooGuedes', '5f65bb29612cf8720d70c568c7a30c55');
-insert into Aluno (Nome, idade, usuario, senha) values('Joao Guedes', 24, 'jooGuedes', '6c1271272217f7bb5da41f5f46f88e05');
+insert into Funcionario (nome, idade, email, usuario, senha) values('Joao Guedes', 24, 'joo_guedes@live.com', 'jooGuedes', '$argon2i$v=19$m=1024,t=2,p=2$amJXMTVKZXBCUExCN0Z2RQ$vuy2Gbmoag71H5T6otHs66eY+tU+3AKDdGhdAkNuzCw');
+insert into Aluno (nome, email, usuario, senha) values('Joao Guedes', 'test@ifrn.edu.br', 'teste', '$argon2i$v=19$m=1024,t=2,p=2$ajg4OWdEb0pKYmVIb2FGUA$XyfOE0U5Is6WzVroGpcuRFfK1xGQspwsfG0KmqSMT9U');
 
 select * from Funcionario;
 select * from Aluno;
