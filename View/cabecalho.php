ca0f6e6f0fadc9b78e87cc5768bc7e4f47a59b25
João Guedes <junior.guedes@academico.ifrn.edu.br>
2019-11-14 17:23:42 -0300

    update 14/11

--- View/cabecalho.php
+++ View/cabecalho.php
@@ -1,23 +1,56 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="stylesheet" href="Design/css/menuLateral.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous">
 </head>
-<body class="bg-light">
-<header class="container">
-    <div class="d-flex my-3 p-3 bg-dark text-white rounded shadow-sm">
-        <h6 class="mr-auto"><?=$titulo?></h6>
-        <?php if(isset($_SESSION['usuario'])){?>
-            <h6>
-                <a href="\logout" class="text-decoration-none text-white">Sair</a>
-            </h6>
-        <?php }?>
-    </div>
-</header>
-<main class="container">
+
+<body class="bg-light" id="main">
+    <header class="container">
+        <?php if (isset($_SESSION['usuario'])) { ?>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+                    Bootstrap
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Alterna navegação">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a class="navbar-brand" href="#">Marca escondida</a>
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Desativado</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+                    </form>
+                    <a href="\logout" class="text-decoration-none">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+                            Logout
+                        </button>
+                    </a>
+                </div>
+            </nav>
+        <?php } else { ?>
+            <div class="d-flex my-3 p-3 bg-dark text-white rounded shadow-sm">
+                <h6 class="mr-auto"><?= $titulo ?></h6>
+            </div>
+        <?php } ?>
+    </header>
+    <main class="container">
