1f2fd6786fa4d490feca9562eca0f86898439355
João Guedes <junior.guedes@academico.ifrn.edu.br>
2019-12-09 21:35:00 -0300

    Atualizando entrada do cpf no login-form

--- View/login-form.php
+++ View/login-form.php
@@ -2,7 +2,7 @@
     <div class=>
         <div class="form-group">
             <label for="exampleInputEmail1">CPF</label>
-            <input type="text" name="cpf" class="form-control" id="exampleInputEmail1" placeholder="Insira seu CPF">
+            <input type="text" name="cpf" maxlength="14" OnKeyPress="formatar('###.###.###-##', this)" class="form-control" id="exampleInputEmail1" placeholder="Insira seu CPF">
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Senha</label>
--- public/Design/js/menuLateral.js
+++ public/Design/js/menuLateral.js
@@ -11,3 +11,11 @@ function closeOpenNav() {
         check--;
     }
 }
+function formatar(mascara, documento){
+    var i = documento.value.length;
+    var saida = mascara.substring(0,1);
+    var texto = mascara.substring(i)
+    if (texto.substring(0,1) != saida){
+        documento.value += texto.substring(0,1);
+    }
+}
