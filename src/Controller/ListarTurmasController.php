7516a218f7077f58d473eeff004a7b06d92f4dfa
João Guedes <junior.guedes@academico.ifrn.edu.br>
2020-01-17 00:38:37 -0300

    Testando

---
+++ src/Controller/ListarTurmasController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ifnc\Tads\Controller;
+
+use Ifnc\Tads\Entity\Turma;
+use Ifnc\Tads\Helper\Render;
+use Ifnc\Tads\Helper\Transaction;
+
+class TurmasController implements IController
+{
+    public function request(): void
+    {
+        Transaction::open();
+        
+        echo Render::html(
+            [
+                "cabecalho.php",
+                "menu.php",
+                "lista-turmas.php",
+                "rodape.php"
+            ],
+            [
+                "type" => $_SESSION["type"],
+                "usuario" => $_SESSION["usuario"],
+                "turmas" => Turma::findByConditionAll("idAluno='{$_SESSION['usuario']->id}'")  
+            ]
+        );
+    }
+}
